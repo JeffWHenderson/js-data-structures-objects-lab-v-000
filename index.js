86320227833014c73e771b0195f79282fcff1273
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-12 22:45:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   driver[key] = value
   return driver
 }
+
+deleteFromDriverByKey(driver, key) {
+  driver.key.delete
+  return driver
+}
